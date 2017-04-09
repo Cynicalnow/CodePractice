67ae242c1d56faf5460a3dbdfc27819a082facd7
Cynicalnow <cynical.wf.wang@gmail.com>
2017-04-09 13:54:49 +0800

    Add MovePdf2RstFolder
    This file read ToMove.txt from source folder and copy files to result
    folder with timestamp

---
+++ Python/Productivity/MovePdf2RstFolder.py
@@ -0,0 +1,35 @@
+import os
+import time
+import shutil
+
+timestamp=timestamp=str(time.localtime(time.time())[3])+\
+           '-'+str(time.localtime(time.time())[4])+\
+           '-'+str(time.localtime(time.time())[5])
+srcDir =input("Please provide the Source File Folder:\n").strip()
+dstDir=os.path.join(srcDir,"Result"+"_"+timestamp)
+os.mkdir(os.path.join(srcDir,"Result"+"_"+timestamp))
+extension=".sldprt"
+ListFile=os.path.join(srcDir,"ToMove.txt")
+
+#Read "ToMove.txt" from source Folder!
+def readList(ListFile):
+   with open(ListFile) as f:
+      fileNames=f.readlines()
+   fileList=[]
+   for item in fileNames:
+      fileList.append(item.strip("\n"))
+   return fileList
+
+def MovePdf2RstFolder(fileList,srcDir,dstDir):
+   for item in fileList:
+      shutil.copy(srcDir+ os.path.sep + item,dstDir+ os.path.sep + item)
+
+fileList=readList(ListFile)
+fileList=(item+extension for item in fileList)
+MovePdf2RstFolder(fileList,srcDir,dstDir)
+
+print("Done!")
+
+
+
+
